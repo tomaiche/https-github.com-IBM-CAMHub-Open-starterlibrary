aa0b50e5238de43955b24aec8a6734e8d6fb7d31
tomaiche <thomas_aiche@fr.ibm.com>
2018-11-29 16:15:51 +0100

    Update main.tf

--- VMware/terraform/hcl/Windows/Chef/main.tf
+++ VMware/terraform/hcl/Windows/Chef/main.tf
@@ -210,20 +210,17 @@ resource "vsphere_virtual_machine" "vm_1" {
   connection {
     type     = "winrm"
     host     = "${var.vm_1_ipv4_address}"
-#    use_ntlm = "true"
-#    insecure = "true"
-#    https    = "false"
     user     = "Administrator"
     password = "TecTec123"
   }
-#  provisioner "file" {
-#    destination = "TomWitnessFile"
-#
-#    content = <<EOF
-#
-# Tom says Hello !
-#
-# EOF
-#
-#    }
+  provisioner "file" {
+    destination = "TomWitnessFile"
+
+    content = <<EOF
+
+ Tom says Hello !
+
+ EOF
+
+    }
   }
